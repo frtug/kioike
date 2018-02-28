97fcb4d12e88faa73bdf33bbf3dc7bd932ec5e03
Abhishek sharma <sharma.abhi3199@gmail.com>
2018-02-28 15:07:53 +0530

    2nd commit

---
+++ Alogrithum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+//fid the square root of the function by using alogrithim
+
+
+	int main(){
+		float f,t,i;
+		cout<<"enter the number to which you want to find the root of that \n";
+		cin>>t;
+		cout<<"\n enter the guess ";
+		cin>>f;
+		for(i=0;i<t;i++){
+			if(f*f==t){
+			cout<<"Answer is"<<f;
+			break;
+		}
+		else{
+			f=(f+t/f)/2;
+			
+		}
+	}
+	cout<<"Ends at ";
+	return 0;
+}
